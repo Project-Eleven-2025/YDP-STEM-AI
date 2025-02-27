b44ebd0ecd6fc0cbda271bfb49e70bf40869f4b3
Richard Villareal <38414584+revdeluxe@users.noreply.github.com>
2025-02-28 03:38:44 +0800

    v0.8

--- process/register.php
+++ process/register.php
@@ -1,74 +1,91 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 session_start();
 
 $host = "localhost";
-$dbname = "masterlist_db";  // Make sure this is correct
-$user = "your_database_user";
-$password = "your_database_password";
+$dbname = "master";  // Make sure this is correct
+$user = "user";
+$password = "123";
 
 try {
-    // Try to connect to PostgreSQL
-    $dsn = "pgsql:host=$host;dbname=$dbname";
+    // Try to connect to MySQL
+    $dsn = "mysql:host=$host;dbname=$dbname";
     $conn = new PDO($dsn, $user, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $dbType = 'PostgreSQL';
 } catch (PDOException $e) {
-    try {
-        // If PostgreSQL connection fails, try to connect to MySQL
-        $dsn = "mysql:host=$host;dbname=$dbname";
-        $conn = new PDO($dsn, $user, $password);
-        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        $dbType = 'MySQL';
-    } catch (PDOException $e) {
-        echo "Could not connect to any database server: " . $e->getMessage();
-        die();
-    }
+    echo "Could not connect to the database: " . $e->getMessage();
+    die();
 }
 
 if (isset($_POST['submit'])) {
-    $fname = $_POST['fname'];
-    $lname = $_POST['lname'];
     $username = $_POST['username'];
     $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
-    $userbirthday = $_POST['userbirthday'];
-    $email = $_POST['email'];  // Get the email input
-    $group = $_POST['group']; // Get the selected group
+    $email = $_POST['email'];
+    $phone = $_POST['phone'];
+    $fname = $_POST['fname'];
+    $lname = $_POST['lname'];
+    $mname = $_POST['mname'] ?? NULL;
+    $nickname = $_POST['nickname'] ?? NULL;
+    $birthdate = $_POST['birthdate'];
+    $address = $_POST['address'];
+    $gender = $_POST['gender'];
+    $school = $_POST['school'];
+    $group = $_POST['group'];
     
     // Generate User ID
     $year = date("Y");
     $month_day = date("md"); // Get MMDD
-    $userIndex = str_pad(rand(0, 9999999999), 10, "0", STR_PAD_LEFT); // 10-digit random number
+    $userIndex = str_pad(rand(0, 9999999999), 10, "0", STR_PAD_LEFT);
     $uniqueID = rand(1000, 9999); // Unique control number (random 4-digit)
     $userID = "{$year}-{$group}{$userIndex}-{$uniqueID}-{$month_day}";
 
-    $sql = "INSERT INTO masterlist (userID, fname, lname, username, password, userbirthday, email) 
-            VALUES (:userID, :fname, :lname, :username, :password, :userbirthday, :email)";
-    $stmt = $conn->prepare($sql);
-    $stmt->execute([
-        ':userID' => $userID,
-        ':fname' => $fname,
-        ':lname' => $lname,
-        ':username' => $username,
-        ':password' => $password,
-        ':userbirthday' => $userbirthday,
-        ':email' => $email // Include email in the query
-    ]);
+    $sql = "INSERT INTO user_info (
+                userID, username, pass_hash, user_emailadd, user_phonenum, user_fname, 
+                user_lname, user_mname, user_nickname, user_birthdate, user_address, user_gender, user_school
+            ) 
+            VALUES (
+                :userID, :username, :pass_hash, :email, :phone, :fname, 
+                :lname, :mname, :nickname, :birthdate, :address, :gender, :school
+            )";
 
-    // Redirect to login.html after successful registration
+    $stmt = $conn->prepare($sql);
+    try {
+        $stmt->execute([
+            ':userID' => $userID,
+            ':username' => $username,
+            ':pass_hash' => $password,  // Fixed: Should match `pass_hash`
+            ':email' => $email,
+            ':phone' => $phone,  // Fixed: Should match `user_phonenum`
+            ':fname' => $fname,
+            ':lname' => $lname,
+            ':mname' => $mname,
+            ':nickname' => $nickname,
+            ':birthdate' => $birthdate,  // Fixed: Corrected `$userbirthday`
+            ':address' => $address,
+            ':gender' => $gender,
+            ':school' => $school
+        ]);
+    } catch (PDOException $e) {
+        die("SQL Error: " . $e->getMessage());
+    }
+    
+    // Redirect after successful registration
     header("Location: ../login.html");
     exit();
 }
 
-// Handle user deletion
-if (isset($_POST['delete'])) {
-    $userID = $_POST['userID'];
-    $sql = "DELETE FROM masterlist WHERE userID = :userID";
-    $stmt = $conn->prepare($sql);
-    $stmt->execute([':userID' => $userID]);
-}
 
 // Fetch all users to display in the table
-$query = $conn->query("SELECT * FROM masterlist");
+$query = $conn->query("SELECT * FROM user_info");
 $users = $query->fetchAll(PDO::FETCH_ASSOC);
 ?>
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 
+echo "<pre>";
+print_r($_POST);
+echo "</pre>";
+exit();
+?>
